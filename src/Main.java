a46ab52f9c11e70d601520cd3b7d6cc2026c7edb
Anton Malkov <malkoff77@yandex.ru>
2023-07-08 16:30:33 +0300

    add code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.prefs.PreferenceChangeListener;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        int amount = 100;                 // сумма на счету
+        int replenishment = 1500;         // пополнение
+        int x = amount + replenishment;  // итого на счету
+        boolean bonus = true;
+
+        int bonus;
+        if (replenishment >= 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+
+        System.out.println(" Итоговый счет: " + (x + bonus));  // вывести на экран итоговую сумму
+        System.out.println(" Количество бонусных рублей: " + bonus); // вывести на экран сумму бонусов
+
     }
+
 }
