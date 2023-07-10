61f259f574c54c9d462e3b51a3a52c1fed918b4c
Anton Malkov <malkoff77@yandex.ru>
2023-07-10 21:19:05 +0300

    code replacement

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,15 @@ import java.util.prefs.PreferenceChangeListener;
 public class Main {
     public static void main(String[] args) {
         int amount = 100;                 // сумма на счету
-        int replenishment = 1500;         // пополнение
-        int x = amount + replenishment;  // итого на счету
-        boolean bonus = true;
+        int replenishment = 2000;         // пополнение
+        int result = amount + replenishment;  // итого на счету
+        int bonus = 0;
 
-        int bonus;
         if (replenishment >= 1000) {
             bonus = replenishment / 100;
-        } else {
-            bonus = 0;
         }
 
-        System.out.println(" Итоговый счет: " + (x + bonus));  // вывести на экран итоговую сумму
+        System.out.println(" Итоговый счет: " + (result + bonus));  // вывести на экран итоговую сумму
         System.out.println(" Количество бонусных рублей: " + bonus); // вывести на экран сумму бонусов
 
     }
